db9daa386fcbbb9c0941d820c47706e2c3d73c26
it19180458 Adikaram <88156209+it19180458@users.noreply.github.com>
2021-07-29 17:30:53 +0000

    I have written tutorial exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  double num1, num2, total, avg;
+
+  printf("Input your mark: \n");
+  scanf("%lf", &num1);
+  printf("Input your mark: \n");
+  scanf("%lf", &num2);
+
+  total = num1 + num2;
+  avg = total / 2;
+
+  printf("Avarage is %lf", avg);
   
   return 0;
 }
